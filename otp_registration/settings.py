ef42beab036ee93c6b97a24513e67d0d776c8696
Telugu Mohan <mohantelugu2003@gmail.com>
2024-08-31 12:19:44 +0530

    updated allowed hosts in settings.py

--- otp_registration/settings.py
+++ otp_registration/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'mobile-mail-otp-django-3.onrender.com',
+]
+
 
 
 # Application definition
